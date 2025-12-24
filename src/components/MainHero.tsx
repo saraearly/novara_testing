d9b71802f31e224d63bebf2ec3982df4d81a01c1
saraearly <saraearly15@gmail.com>
2025-12-24 14:15:35 -0700

    added blue gradient

--- src/components/MainHero.tsx
+++ src/components/MainHero.tsx
@@ -8,18 +8,19 @@ const MainHero = () => {
     <div>
       {/* Title */}
       <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-white tracking-tight font-bold leading-tight">
-        {/* First line: always one line on large screens */}
+        {/* First line */}
         <span className="inline-block whitespace-nowrap">
           {mainHero.title.replace("with NOVARA", "")}
         </span>
 
-        {/* Second line: always 'with NOVARA' */}
+        {/* Second line */}
         <span className="block">
           with{" "}
           <span
             className="inline-block bg-clip-text text-transparent"
             style={{
-              backgroundImage: "linear-gradient(90deg, #7B2FF7 0%, #18D3C5 100%)",
+              backgroundImage:
+                "linear-gradient(90deg, #7B2FF7 0%, #18D3C5 100%)",
             }}
           >
             NOVARA
