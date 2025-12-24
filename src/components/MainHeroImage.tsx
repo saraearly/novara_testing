4726dcc247e37b2cdeb120df0ddd27f44e0cac10
saraearly <saraearly15@gmail.com>
2025-12-24 11:28:15 -0700

    added blue gradient

--- src/components/MainHeroImage.tsx
+++ src/components/MainHeroImage.tsx
@@ -1,16 +1,19 @@
 import React from "react";
 import config from "../config/index.json";
 
-const MainHeroImage = () => {
+type MainHeroImageProps = {
+  className?: string;
+};
+
+const MainHeroImage = ({ className }: MainHeroImageProps) => {
   const { mainHero } = config;
+
   return (
-    <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2 z-0">
-      <img
-        className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
-        src={mainHero.img}
-        alt="Hero image"
-      />
-    </div>
+    <img
+      src={mainHero.img}
+      alt="Hero image"
+      className={className}
+    />
   );
 };
 
