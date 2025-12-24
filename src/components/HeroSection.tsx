40c26142f54247eb7c32e3193bab4b051dd39aa6
saraearly <saraearly15@gmail.com>
2025-12-24 11:30:20 -0700

    added blue gradient

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -9,20 +9,19 @@ const HeroSection = () => (
     <Header />
 
     <section className="relative grid grid-cols-1 lg:grid-cols-2 w-full overflow-hidden">
-    {/* Left side (text + gradient) */}
-    <div className="relative flex items-center px-6 py-12 lg:pl-16">
+      {/* Left side (text + gradient) */}
+      <div className="relative flex items-center px-6 py-12 lg:pl-16">
         <div className="absolute inset-0 bg-gradient-to-r from-[#0f2a44] to-transparent pointer-events-none" />
         <div className="relative z-10 max-w-lg mx-auto lg:mx-0 text-center lg:text-left">
-        <MainHero />
+          <MainHero />
         </div>
-    </div>
+      </div>
 
-    {/* Right side (image) */}
-    <div className="h-[300px] lg:h-[600px]">
+      {/* Right side (image) */}
+      <div className="h-[300px] lg:h-[600px]">
         <MainHeroImage className="w-full h-full object-cover" />
-    </div>
+      </div>
     </section>
-
   </div>
 );
 
